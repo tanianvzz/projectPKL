62ff27c71fc82216178bbd4c47100f0430418a76
rifan-t <rifanzprr@gmail.com>
2023-12-19 12:22:07 +0700

    member 2

--- application/models/M_user1.php
+++ application/models/M_user1.php
@@ -3,5 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_user1 extends CI_Model {
 
+    public function SemuaData(){
+       return $this->db->get('user');
+    }
+    public function get_user($user_id){
+        return $this->db->get_where('user', array('id'=> $user_id));
+     }
+
     
 }
