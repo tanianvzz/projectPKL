c632544cef9601b30049b992b5efad18c6819f55
sitilulu <sitiluluilma883@gmail.com>
2023-12-27 21:44:13 -0800

    tambah logo

--- application/controllers/Datauser2.php
+++ application/controllers/Datauser2.php
@@ -11,22 +11,22 @@ class Datauser2 extends CI_Controller {
 	}	
 	public function index()
 	{
-		if (!$this->session->userdata('logged_in')) {
-			redirect('auth');
-		}
+		// if (!$this->session->userdata('logged_in')) {
+		// 	redirect('auth');
+		// }
 		$user_id = $this->session->userdata('user_id');
-        $data['user'] = $this->user_model->get_user($user_id);
+        $data['user'] = $this->M_user1->get_user($user_id);
 		$this->load->view('user2/header', $data);
 		$this->load->view('user2/user2', $data);
 		$this->load->view('user2/footer', $data);
 	}
 	public function Myprofile()
 	{
-		if (!$this->session->userdata('logged_in')) {
-			redirect('auth');
-		}
+		// if (!$this->session->userdata('logged_in')) {
+		// 	redirect('auth');
+		// }
 		$user_id = $this->session->userdata('user_id');
-        $data['user'] = $this->user_model->get_user($user_id);
+        $data['user'] = $this->M_user1->get_user($user_id);
 		$this->load->view('user2/header');
 		$this->load->view('user2/user_profile2', $data);
 		$this->load->view('user2/footer');
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,52 +1,48 @@
 <div class="container">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-            <div class="col-6">
+        <div class="col-8">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">LOGIN PAGE!</h1>
-                                    </div>
-                                    <?= $this->session->flashdata('message'); ?>
-                                    <form class="user" method="post" action="<?= base_url('auth');?>" >
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                id="email" name="email" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
-                                       <?= form_error('email', '<small class="text-danger pl-3">','</small>');?>
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-3">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-2">
+                                <div class="text-center">
+                                    <img src="<?= base_url('assets/img/logo1.png'); ?>" class="w-25 rounded-circle ">
+                                </div>
+                                <?= $this->session->flashdata('message'); ?>
+                                <form class="user" method="post" action="<?= base_url('auth'); ?>">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" id="email"
+                                            name="email" aria-describedby="emailHelp"
+                                            placeholder="Enter Email Address...">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
 
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="password" name="password"  placeholder="Password">
-                                       <?= form_error('password', '<small class="text-danger pl-3">','</small>');?>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </button>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
-                                    <div class="text-center">
-                                        <a class="small" href="<?= base_url('auth/registration'); ?>">Create an Account!</a>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password"
+                                            name="password" placeholder="Password">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                </form>
+                                <div class="text-center">
+                                    <a class="small" href="<?= base_url(' /registration'); ?>">Create an Account!</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
+
+    </div>
 
 </div>
